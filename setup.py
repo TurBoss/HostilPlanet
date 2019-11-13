c323f21ee6f72f1d72ed8ca6754326d5a2f89617
TurBoss <j.l.toledano.l@gmail.com>
2019-11-13 09:06:33 +0100

    pythooon setup not cxfreeze

--- setup.py
+++ setup.py
@@ -1,38 +1,60 @@
-import sys
-from cx_Freeze import setup, Executable
+from distutils.core import Extension, setup
+from distutils.command.install import INSTALL_SCHEMES
+import os
 
-# Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {
-    "packages": ["os", "pygame"],
-    "excludes": ['_gtkagg', '_tkagg', 'bsddb', 'curses', 'email', 'pywin.debugger', 'pywin.debugger.dbgcon',
-                 'pywin.dialogs', 'tcl', 'Tkconstants', 'Tkinter'],
-    "include_files": ["data"]
-}
+# http://stackoverflow.com/questions/1612733/including-non-python-files-with-setup-py
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
 
-# GUI applications require a different base on Windows (the default is for a
-# console application).
-base = None
 
-if sys.platform == "win32":
-    base = "Win32GUI"
+def find_data_files(srcdir, *wildcard):
+    file_list = []
+    if not srcdir.endswith('/'):
+        srcdir += '/'
+    for files in os.listdir(srcdir):
+        if files.endswith(wildcard):
+            file_list.append(srcdir + files)
+    return file_list
 
-target = Executable(
-    script="run_game.pyw",
-    base=base,
-    targetName="HostilPlanet.exe",
-    compress=False,
-    copyDependentFiles=True,
-    appendScriptToExe=True,
-    appendScriptToLibrary=False,
-    icon="icon.ico",
-    shortcutName="Hostil Planet",
-    shortcutDir="DesktopFolder",
+
+bkgr = find_data_files('data/bkgr/', '.png')
+chips = find_data_files('data/chips/', '.png')
+ending = find_data_files('data/ending/', '.png')
+images = find_data_files('data/images/', '.png')
+intro = find_data_files('data/intro/', '.png')
+statusbar = find_data_files('data/statusbar/', '.png')
+
+
+levels = find_data_files('data/levels/', '.tga')
+music = find_data_files('data/music/', '.ogg')
+sfx = find_data_files('data/sfx/', '.ogg')
+
+setup(name='HostilPlanet',
+      version='1.0.0',
+      author='turboss',
+      author_email='turboss@mail.com',
+      description='Plaform robot game',
+      url='http://github.com/TurBoss/HostilPlanet',
+      download_url='http://github.com/TurBoss/HostilPlanet',
+      license='GPL license',
+      long_description=open('README.md').read(),
+      scripts=['run_game.py'],
+      packages=['lib', 'lib.pgu'],
+      data_files=[('data/fonts', ['data/fonts/04B_20__.TTF']),
+                  ('data/bkgr', bkgr),
+                  ('data/chips', chips),
+                  ('data/ending', ending),
+                  ('data/images', images),
+                  ('data/intro', intro),
+                  ('data/statusbar', statusbar),
+
+                  ('data/levels', levels),
+                  ('data/music', music),
+                  ('data/sfx', sfx),
+                  ],
+      requires=[
+          "pygame",
+          "numpy",
+          "pyrex",
+      ],
 )
-
-setup(name="Hostil Planet",
-      version="0.1",
-      author="Jauria Studios",
-      description="Hostil Planet by Jauria Studios",
-      options={"build_exe": build_exe_options},
-      executables=[target]
-      )
